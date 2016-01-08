23a71d63d041ca62e122e184b57fadf705ad116f
Dan <solovyev.daniel@gmail.com>
2016-01-08 18:23:58 +0200

    add auth (not work)

--- ATM/urls.py
+++ ATM/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'ATM'
 urlpatterns = [
+    url(r'^auth/$', views.login, name='login'),
     url(r'^$', views.index, name='index'),
     # url(r'^$', views.IndexView.as_view(), name='index'),
 ]
--- ATM/views.py
+++ ATM/views.py
@@ -4,47 +4,42 @@ from django.contrib.auth.models import User
 from django.template.context_processors import csrf
 
 
-# from django.views import generic
-
-
-# class IndexView(generic.CreateView):
-#     template_name = 'main.html'
-
-
-# def index(request):
-#     args = {}
-#     args.update(csrf(request))
-#     if request.POST:
-#         username = request.POST['username']
-#         password = request.POST.get('password', '')
-#         user = auth.authenticate(username=username, password=password)
-#         if user is not None:
-#             auth.login(request, user)
-#             args['username1'] = username
-#             return render_to_response('main.html', args)
-#         else:
-#             args['login_error'] = 'no user'
-#             return render_to_response('main.html', args)
-#     else:
-#         return render_to_response('main.html', args)
-
 def index(request):
     args = {}
     args.update(csrf(request))
-    if request.POST:
-        cardnumber = request.POST['cardnumber']
+    if request.GET:
+        cardnumber = request.GET['cardnumber']
         try:
             card = User.objects.get(username=cardnumber)
         except User.DoesNotExist:
-            args['login_error'] = 'no user'
+            args['login_error'] = 'no user in db'
             return render_to_response('main.html', args)
         else:
-            if card.is_active == 1:
-                #tyt zamenit na zapis v session
-                args['username1'] = card.username + ' activ'
-                return render_to_response('main.html', args)
+            if card.is_active:
+                # session or arg?
+                request.session.set_expiry(30)
+                request.session['card_number'] = card.username
+                return redirect('auth/')
             else:
-                args['username1'] = card.username + ' passiv'
+                args['username'] = card.username + ' deactivated'
                 return render_to_response('main.html', args)
     else:
         return render_to_response('main.html', args)
+
+
+def login(request):
+    args = {}
+    args.update(csrf(request))
+    if request.POST:
+        username = request.session.get('card_number')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            args['username'] = username + 'logged in'
+            return render_to_response('login.html', args)
+        else:
+            args['login_error'] = 'no user'
+            return render_to_response('login.html', args)
+    else:
+        return render_to_response('login.html')
